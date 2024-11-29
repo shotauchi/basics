a17b3d986a4f2a052474964babd2d48a2bf1d19e
[shotauchi] <[tianneixiang84@gmail.com]>
2024-11-29 19:51:08 +0000

    laraver09~11

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -25,5 +27,31 @@ class ProfileController extends Controller
     public function update()
     {
         return redirect('admin/profile/edit');
+    $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
+        return redirect('admin/profile/create');
+    }
+    
+       public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
     }
 }
